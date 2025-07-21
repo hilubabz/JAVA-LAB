44697fdbbe0cf7138fcf9f638b7cca1a9ade327a
hilubabz <121779738+hilubabz@users.noreply.github.com>
2025-07-21 08:57:16 +0545

    Create Lab2.java

---
+++ Lab2.java
@@ -0,0 +1,66 @@
+// Base class
+class Shape {
+    // Method to get area - to be overridden by subclasses
+    public double getArea() {
+        return 0;
+    }
+}
+
+// Rectangle subclass
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+}
+
+// Circle subclass
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Triangle subclass
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * base * height;
+    }
+}
+
+// Test class with main method
+public class Lab2 {
+    public static void main(String[] args) {
+        Shape rect = new Rectangle(10, 5);
+        Shape circle = new Circle(7);
+        Shape triangle = new Triangle(8, 6);
+
+        System.out.println("Area of Rectangle: " + rect.getArea());
+        System.out.println("Area of Circle: " + circle.getArea());
+        System.out.println("Area of Triangle: " + triangle.getArea());
+    }
+}
